3bab59b7463aea9a6a54bb81680c2a236014b75b
WinXP655 <pronin11-nik@yandex.ru>
2026-05-27 22:37:31 +0300

    Add files via upload

---
+++ fish.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(void) {
+    printf("fish\n");
+    const char *brick[] = {"><(((o>   <o)))><   ><(((o>"};
+    for (int i = 0; i < 5; i++) puts(brick[i]);
+    return 0;
+}
